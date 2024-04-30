94d3b54f5db127f81c7d87c3d275d60bd7291a54
Thomas2710 <thomas.trevisan@studenti.unitn.it>
2024-04-30 14:16:41 +0200

    processed data added

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ interesting_columns = ['iso_code', 'date','total_cases', 'new_cases', 'total_dea
 
 datapath = '/data'
 raw_file = '/raw.csv'
+recoveries_file = '/recovery.csv'
 refined_file = '/refined.csv'
 
 
@@ -18,25 +19,36 @@ def get_data(filepath):
     df = pd.read_csv(current_path+filepath)
     return df
 
+def get_missing_dates(df):
+    #Finding holes in the dates with some missing values
+    df['date'] = pd.to_datetime(df['date'], format = 'mixed')
+    start_date = df.iloc[0].loc['date']
+    end_date = df.iloc[-1].loc['date']
+    date_range = pd.date_range(start_date, end_date)
+    df.reindex(date_range).isnull().all(1)
+    diff_range = date_range.difference(df['date'])
+    return diff_range
+
+def insert_missing_dates(df):
+    df['date'] = pd.to_datetime(df['date'], format = 'mixed')
+    start_date = df.iloc[0].loc['date']
+    end_date = df.iloc[-1].loc['date']
+    date_range = pd.date_range(start_date, end_date)
+    df.set_index('date').reindex(date_range)
+    print(get_missing_dates(df))
+    return df
 
 df = get_data(datapath+raw_file)
+df_recoveries = get_data(datapath+recoveries_file)
+missing_recovery_dates = get_missing_dates(df_recoveries)
+print(missing_recovery_dates)
+
 
 #Saving italian rows without null
 df_ita = df[df['location'] == country]
 df_ita_filtered = df_ita[interesting_columns].dropna()
 df_ita_filtered.to_csv(os.getcwd()+datapath+refined_file)
-
-
-#Finding holes in the dates with some missing values
-df_ita_filtered['date'] = pd.to_datetime(df_ita_filtered['date'])
-start_date = df_ita_filtered.iloc[0].loc['date']
-end_date = df_ita_filtered.iloc[-1].loc['date']
-
-
-date_range = pd.date_range(start_date, end_date)
-df_ita_filtered.reindex(date_range
-                        ).isnull().all(1)
-diff_range = date_range.difference(df_ita_filtered['date'])
-print(diff_range)
+missing_ita_dates = get_missing_dates(df_ita_filtered)
+print(missing_ita_dates)
 
 #Further investigation on missing values for those rows 
